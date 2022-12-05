cbbbe3125abbb81dd588bf850df0cd84bab18ea3
Artyom <simstemasss@mail.ru>
2022-12-05 21:26:15 +0300

    Second task

---
+++ 2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите значение точки A");
+int x1= int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значине точки B");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+double result =Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2));
+System.Console.WriteLine("Расстояние между точкой А и точкой B = " + result);
